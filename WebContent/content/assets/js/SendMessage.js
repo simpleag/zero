f4dc8d047ee462c0b91a2ae96bce40e146b874e8
Leo Hu <lalalaleo.hu@hotmail.com>
2017-04-22 23:10:16 +0800

    前端页面接手，重新整理
    .page高度修改，页面布局修正。
    各元素边距微调。
    success页修改为state页，动态调用，显示action结果。
    教师信息页面，修改了排版和内容。

--- WebContent/content/assets/js/SendMessage.js
+++ WebContent/content/assets/js/SendMessage.js
@@ -2,7 +2,7 @@
           document.getElementById("Student").style.display="none";
   });
 function Class(argument){
-    alert($(argument).children(".weui-cell__bd").children("p").text());
+    // alert($(argument).children(".weui-cell__bd").children("p").text());
     document.getElementById("SnedMessage").style.display="none";
     document.getElementById("Student").style.display="block";
 }
